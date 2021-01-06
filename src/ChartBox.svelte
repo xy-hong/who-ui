34263b994b44aa5421d36876ffaff2fba3a0d409
Heo <name.xy.hong@gmail.com>
2021-01-06 23:20:52 +0800

    使用 ts

---
+++ src/ChartBox.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    // let nick = prompt('输入你的昵称:')
+    let nick = 'hxy'
+    let records = [
+        { nick: 'hxy', message: '你好(*´▽｀)ノノ', time: '22:50'},
+        { nick: 'zhh', message: '(*´▽｀)ノノ哟哟哟', time: '22:56'},
+        { nick: 'zhh', message: '你从哪来来', time: '22:58'},
+        { nick: 'hxy', message: '新世界来的', time: '22:59'},
+    ]
+</script>
+
+<div id="chart-container">
+    <div>
+        <div id="chart-box">
+            {#each records as record}
+            <div>{record.time} {record.nick}: {record.message}</div>
+            {/each}
+        </div>
+        <div>
+            <input />
+            <button>SEND</button>
+        </div>
+    </div>
+</div>
+
+<style>
+    #chart-container {
+        display: flex;
+        justify-content: center;
+    }
+
+    #chart-box {
+        background-color: whitesmoke;
+        border: 1px solid gray;
+    }
+</style>
+
---
+++ src/Home.svelte
@@ -0,0 +1,26 @@
+<div class="out-box">
+    <div>
+        <span class="title">who is the spy!</span>
+        <p>这里将是一系列的规则介绍</p>
+        <ul>
+            <li>规则一</li>
+            <li>规则二</li>
+            <li>规则三</li>
+            <li>规则四</li>
+            <li>规则五</li>
+        </ul>
+    </div>
+</div>
+<style>
+    .out-box {
+		display: flex;
+        justify-content: center;
+	}
+
+    .title {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+</style>
